9a9a3788b8438bdcdced35c40669211703fd33fa
hakaboom <43911741+hakaboom@users.noreply.github.com>
2020-12-25 17:04:21 +0800

    Update main.py

--- test/main.py
+++ test/main.py
@@ -1,47 +1,24 @@
-from selenium import webdriver
-from base.behavior_Tree import Sequence
-from base.behavior_Tree import Blackboard
-import time
+from base.appium_config import DriverClient
+from time import sleep
+import os
+import re
 
+class sceen:
+    def __init__(self):
+        self.driver = DriverClient().getDriver()
+        print(self.driver.current_activity)
+    def getSize(self):
+        return self.driver.get_window_size()
 
-Main = Blackboard()
-Main.setValueBath({
-    'count' : 0,
-})
-start = Sequence()
-start.setLoop(True,-1,-1,1)
-start_err = Main.createScene()
-start_suc = Main.createScene()
-start.addScene(start_suc)
-start.addScene(start_err)
-def suc(_):
-    global Main
-    count = Main.getValue('count') + 1
-    print(count)
-    Main.setValue('count',count)
-    if count >=5:
-        print('suc')
-        return True
-def err(_):
-    pass
-start_suc.getStartTrigger().setRule(suc)
-start_err.getStartTrigger().setRule(err)
+    def getDPI(self):
+        return self.driver.get
 
-
-
-suc_login = Sequence()
-suc_login.setLoop(True,3,-1,1)
-start_suc.addSequence(suc_login) '''向start_suc增加情景,满足star_suc后会运行suc_login里'''
-suc_login_suc = Main.createScene()
-suc_login_err = Main.createScene()
-suc_login.addScene(suc_login_suc)
-suc_login.addScene(suc_login_err)
-def suc_login(blackboard):
-    print('uc_login')
-def err_login(blackboard):
-    print('err_login')
-suc_login_suc.getStartTrigger().setRule(suc_login)
-suc_login_err.getStartTrigger().setRule(err_login)
-
-
-start.run()
+class system:
+    def __init__(self):
+        self.driver = DriverClient().getDriver()
+    def runApp(self,appName,appActivity):
+        self.driver.start_activity(appName, appActivity)
+b=sceen()
+print(b.getSize())
+b=system()
+b.runApp('com.bilibili.priconne', 'com.bilibili.princonne.permission.PermissionActivity')
