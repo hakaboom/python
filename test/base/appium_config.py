6487a2a04e70ae924f2ae5d9dfd3912cf43e864d
hakaboom <43911741+hakaboom@users.noreply.github.com>
2020-12-26 20:07:02 +0800

    u2
    不用appium,换成uiautomator2

--- test/base/appium_config.py
+++ test/base/appium_config.py
@@ -23,3 +23,37 @@ class Singleton(object):
 class DriverClient(Singleton):
     def getDriver(self):
         return self.driver
+
+'''
+    from base.appium_config import DriverClient
+    from time import sleep
+    import os
+    import re
+
+    class sceen:
+        def __init__(self):
+            self.driver = DriverClient().getDriver()
+            print(self.driver.current_activity)
+
+        def getSize(self):
+            return self.driver.get_window_size()
+
+        def getDPI(self):
+            return self.driver.get
+
+        def snapshot(self):
+            self.driver.save_screenshot('screen111.png')
+
+    class system:
+        def __init__(self):
+            self.driver = DriverClient().getDriver()
+
+        def runApp(self, appName, appActivity):
+            self.driver.start_activity(appName, appActivity)
+
+    b = sceen()
+    print(b.getSize())
+    b.snapshot()
+    b = system()
+    b.runApp('com.bilibili.priconne', 'com.bilibili.princonne.permission.PermissionActivity')
+'''
--- test/main.py
+++ test/main.py
@@ -1,24 +1,50 @@
-from base.appium_config import DriverClient
-from time import sleep
-import os
-import re
+import uiautomator2 as u2
 
-class sceen:
-    def __init__(self):
-        self.driver = DriverClient().getDriver()
-        print(self.driver.current_activity)
-    def getSize(self):
-        return self.driver.get_window_size()
 
-    def getDPI(self):
-        return self.driver.get
+_DEVICE_ID = "emulator-5562"
+driver = u2.connect(_DEVICE_ID)
 
+driver.touch.down(100,100)
 class system:
     def __init__(self):
-        self.driver = DriverClient().getDriver()
-    def runApp(self,appName,appActivity):
-        self.driver.start_activity(appName, appActivity)
-b=sceen()
-print(b.getSize())
-b=system()
-b.runApp('com.bilibili.priconne', 'com.bilibili.princonne.permission.PermissionActivity')
+        global driver
+        self.driver = driver
+
+    def runApp(self,appPackage,appActivity=None):
+        self.driver.app_start(appPackage,appActivity)
+
+    def isAppRunning(self,appPackage):
+        try:
+            return self.driver.app_list_running().index(appPackage) != None
+        except:
+            print('App is not running')
+            return False
+
+    def getForegroundApp(self):
+        return driver.app_current()['package']
+
+    def killApp(self,appPackage):
+        self.driver.app_stop(appPackage)
+
+    def setClipboard(self,text):
+        self.driver.set_clipboard(text)
+
+    def getClipboard(self):
+        return self.driver.clipboard
+
+    def getSystemInfo(self):
+        return self.driver.info
+
+class touch:
+    def __init__(self):
+        global driver
+        self.driver = driver
+
+
+    def click(self,x,y,index=1):
+        self.clickIndex[index].press(x,y)
+
+class screen:
+    pass
+
+system = system()
