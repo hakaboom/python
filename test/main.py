32127f39ad0305346e4c66f00fc9044667a0d207
hakaboom <270704881@qq.com>
2020-12-28 00:22:23 +0800

    使用evend操作点击

--- test/main.py
+++ test/main.py
@@ -1,10 +1,18 @@
 import uiautomator2 as u2
+import re
+import time
+from pyminitouch import MNTDevice
+from pyminitouch import safe_connection
+from pyminitouch import CommandBuilder
 
-
-_DEVICE_ID = "emulator-5562"
+_DEVICE_ID = "emulator-5554"
+_DEVICE_ID = "127.0.0.1:5555"
 driver = u2.connect(_DEVICE_ID)
-
-driver.touch.down(100,100)
+driver.touch1.down(520,230,1)
+driver.touch1.down(800,230,2)
+time.sleep(1)
+# driver.touch1.up(520,230,1)
+# driver.touch1.up(650,230,2)
 class system:
     def __init__(self):
         global driver
@@ -47,4 +55,29 @@ class touch:
 class screen:
     pass
 
-system = system()
+
+
+
+
+'''
+
+[     147.083927] /dev/input/event4: 0003 002f 00000000
+[     147.083927] /dev/input/event4: 0003 0039 0000000e
+[     147.083927] /dev/input/event4: 0001 014a 00000001
+[     147.083927] /dev/input/event4: 0003 0035 00001e62
+[     147.083927] /dev/input/event4: 0003 0036 00001a02
+[     147.083927] /dev/input/event4: 0000 0000 00000000
+[     147.983737] /dev/input/event4: 0003 002f 00000001
+[     147.983737] /dev/input/event4: 0003 0039 0000000f
+[     147.983737] /dev/input/event4: 0003 0035 000025d6
+[     147.983737] /dev/input/event4: 0003 0036 00001a2e
+[     147.983737] /dev/input/event4: 0000 0000 00000000
+[     151.152794] /dev/input/event4: 0003 002f 00000000
+[     151.152794] /dev/input/event4: 0003 0039 ffffffff
+[     151.152794] /dev/input/event4: 0000 0000 00000000
+[     154.657181] /dev/input/event4: 0003 002f 00000001
+[     154.657181] /dev/input/event4: 0003 0039 ffffffff
+[     154.657181] /dev/input/event4: 0001 014a 00000000
+[     154.657181] /dev/input/event4: 0000 0000 00000000
+
+'''
