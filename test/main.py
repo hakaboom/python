bbd9d524373fde6cf8072c5ced76828655d00994
hakaboom <43911741+hakaboom@users.noreply.github.com>
2020-12-30 16:54:52 +0800

    增加了范围截取imgData

--- test/main.py
+++ test/main.py
@@ -9,17 +9,23 @@ pip3.8 install opencv-contrib-python -i http://pypi.douban.com/simple --trusted-
 import numpy as np
 import cv2
 #
-# a=time.time()
-# imgData = np.fromfile('screen.raw', dtype=np.uint8)
-# imgData = imgData[slice(12,len(imgData))]
-# imgData = imgData.reshape(720, 1280, 4)
-# print(time.time()-a)
 
-_DEVICE_ID = "emulator-5562"
-#_DEVICE_ID = "127.0.0.1:5555"
-driver = u2.connect(_DEVICE_ID)
+imgData = np.fromfile('script.raw', dtype=np.uint8)
+imgData = imgData[slice(12,len(imgData))]
+imgData = imgData.reshape(720, 1280, 4)
 
-print(driver.screen.getRGB(100, 150))
-time.sleep(2)
+'''
+    imgData范围截取
+    imgData[100:720-200, 100:1280-100]
+    imgData[y:y+长, x:x+宽
+'''
+print(imgData[:200+1,100+1:200+1])
 
-print(driver.screen.getRGB(100, 150))
+# _DEVICE_ID = "emulator-5562"
+# #_DEVICE_ID = "127.0.0.1:5555"
+# driver = u2.connect(_DEVICE_ID)
+#
+# print(driver.screen.getRGB(100, 150))
+# time.sleep(2)
+#
+# print(driver.screen.getRGB(100, 150))
